7fabc9bcaaadd1e291548a410d0b52b070053571
Roman Martemyanov <roman73n87@mail.ru>
2020-02-02 15:28:18 +0300

    Initial MoveController

---
+++ src/ru/mart/xo/controllers/MoveController.java
@@ -0,0 +1,21 @@
+package ru.mart.xo.controllers;
+
+import ru.mart.xo.model.Field;
+import ru.mart.xo.model.Figure;
+import ru.mart.xo.model.exception.AlreadyOccupiedException;
+import ru.mart.xo.model.exception.InvalidPointException;
+
+
+import java.awt.*;
+
+public class MoveController {
+        public void applyFigure (final Field field,
+                                 final Point point,
+                                 final Figure figure) throws InvalidPointException, AlreadyOccupiedException{
+            if (field.getFigure(point)!= null){
+                throw new AlreadyOccupiedException();
+            }
+
+            field.setFigure(point, figure);
+        }
+}
