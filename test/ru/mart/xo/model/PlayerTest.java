2aee177540e966d6a7018d980a14d8475b344f87
Roman Martemyanov <roman73n87@mail.ru>
2020-02-03 02:17:09 +0300

    PlayerTest replaced

--- test/ru/mart/xo/model/PlayerTest.java
+++ test/ru/mart/xo/model/PlayerTest.java
@@ -19,14 +19,27 @@ public class PlayerTest {
         }
 
     @Test
-    public void getFigure() {
-        final Figure inputValue = Figure.X;
-        final Figure expectedValue = inputValue;
+    public void getNameNotNull() {
+        final String inputValue = "Roman";
+        final String expectedValue = "Roman";
 
-        final Player player = new Player(null, inputValue) ;
+        final Player player = new Player(inputValue, null) ;
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getFigure() {
+        final Figure expectedValue = Figure.X;
+
+        final Player player = new Player(null, expectedValue) ;
 
         final Figure actualValue = player.getFigure();
 
         assertEquals(expectedValue, actualValue);
     }
+
+
 }
