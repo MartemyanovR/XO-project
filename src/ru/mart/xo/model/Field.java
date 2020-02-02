03fca8a223887ce8d79a8f30074649c379f27b05
Roman Martemyanov <roman73n87@mail.ru>
2020-02-03 02:00:09 +0300

    Field - replaced by dynamic size.

--- src/ru/mart/xo/model/Field.java
+++ src/ru/mart/xo/model/Field.java
@@ -1,42 +1,44 @@
 package ru.mart.xo.model;
 
 import ru.mart.xo.model.exception.InvalidPointException;
-import ru.mart.xo.model.exception.AlreadyOccupiedException;
 
 import java.awt.*;
 
 public class Field {
 
-    private static final int FIELD_SIZE = 3;
+    private final Figure[][] field;
+    private final int fieldSize;
     private static final int MIN_COORDINATE = 0;
-    private static final int MAX_COORDINATE = FIELD_SIZE;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+     public Field (final int sizeX, final int sizeY) {
+         this.fieldSize = sizeX;
+         field = new Figure[sizeX][sizeY];
 
+     }
     public int getSize() {
-        return FIELD_SIZE;
+        return fieldSize;
     }
 
     public Figure getFigure (final Point point) throws InvalidPointException {
-        if (!checkPoint(point)) {
+        if (isCheckPointer(point)) {
             throw new InvalidPointException();
         }
         return field[point.x][point.y];
     }
 
     public void setFigure( final Point point, final Figure figure) throws InvalidPointException {
-        if (!checkPoint(point)) {
+        if (isCheckPointer(point)) {
             throw new InvalidPointException();
         }
         field[point.x][point.y] = figure;
     }
 
-    private boolean checkPoint(final Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    private boolean isCheckPointer(final Point point) {
+        return isCheckCoordinate(point.x, field.length) || !isCheckCoordinate(point.y, field[point.x].length);
     }
 
-    private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean isCheckCoordinate(final int coordinate, int maxCoordinate) {
+        return (coordinate >= MIN_COORDINATE && coordinate < maxCoordinate);
     }
 
 
