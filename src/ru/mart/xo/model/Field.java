27fcf2cc5f4cf532cda193f53351cc86e3c02425
Roman Martemyanov <roman73n87@mail.ru>
2020-02-10 03:15:31 +0300

    added getSizeX and getSizeY methods

--- src/ru/mart/xo/model/Field.java
+++ src/ru/mart/xo/model/Field.java
@@ -9,14 +9,26 @@ public class Field {
     private final Figure[][] field;
     private final int fieldSize;
     private static final int MIN_COORDINATE = 0;
+    private final int sizeX;
+    private final int sizeY;
 
      public Field (final int sizeX, final int sizeY) {
+         this.sizeX = sizeX;
+         this.sizeY = sizeY;
          this.fieldSize = sizeX;
          field = new Figure[sizeX][sizeY];
 
      }
     public int getSize() {
         return fieldSize;
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
     }
 
     public Figure getFigure (final Point point) throws InvalidPointException {
